396c1f1cb69663853f093b5b20db0254219b02a1
mluthfirr <muhammad_luthfirrahman@apps.ipb.ac.id>
2018-07-10 20:27:43 +0700

    Error Fixed. dan sudah ditambahkan error page

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -8,16 +8,16 @@ class VoucherController extends Controller
 {
     public function get($code, Voucher $voucher)
     {
-        $voucher = $voucher->withCode($code)-> first();
-
+        $voucher = $voucher->withCode($code)->first();
+        // dd($voucher);
         if (!$voucher) {
             abort(404);
         }
 
 
-        return view('voucher', [
-            'voucher' -> $voucher,
-        ]);
+        return view('voucher', compact(
+            'voucher'
+        ));
     }
     
 }
